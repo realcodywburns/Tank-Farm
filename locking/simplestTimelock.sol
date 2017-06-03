9281cdf78cc3a36843f54a5bb8c5b429b4626499
Cody Burns <cody.w.burns@gmail.com>
2017-06-02 21:22:32 -0500

    init

---
+++ locking/simplestTimelock.sol
@@ -0,0 +1,39 @@
+// Simplest Time locking contract
+// @authors:
+// Cody Burns <dontpanic@codywburns.com>
+// license: Apache 2.0
+
+// usage:
+// Set the time before you send the contract. any funds will be locked until that time. 
+// After the lock has expired any contract that sends to it will get all funds
+
+
+
+contract simplestTimelock {
+    
+// global vars
+    uint public releaseTime = 1496423100;       //stores the unix encoded timestamp of release, nothing will be released until this time 
+//mapping    
+
+//events
+    
+//functions    
+
+    //when the lock has expired sending anything to the contract will get all funds sent to whom ever sends first
+    function() payable {
+        require (block.timestamp > releaseTime);
+        msg.sender.transfer(this.balance);
+    }
+
+// safety features
+
+// abi and contract ipfs
+    bool isLocked;
+    string public ipfsAbi;
+
+    function regipfs(string _ipfsAbi) {
+        if(isLocked){throw;}
+        isLocked=true;
+        ipfsAbi = _ipfsAbi;
+    }
+}

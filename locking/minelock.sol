936f0260cea9d87716b6f9346dbbd3f391e44cb7
Cody Burns <cody.w.burns@gmail.com>
2017-06-04 17:55:47 -0500

    v0.1.1
    Added limiter on incoming funds to send any amount over the term of trade back to the sponsor so it is not important if a miner exceeds the contracted amount.

--- locking/minelock.sol
+++ locking/minelock.sol
@@ -12,14 +12,14 @@
 contract minelock {
     
 // global vars
-    uint public releaseTime = 1496423100;   //stores the unix encoded timestamp of release 
-    unit public termOfTrade = 100 ether;    // final contract goal
-    bool public metToT;                            // did contract close
-    uint public listPrice;                  //allow for dynamic pricing
-    address public owner = msg.sender;      // person who is currently able to withdraw
-    address public sponsor = msg.sender;    // person who is putting reputation on the line
-    string public pool;                     // pool url the contract can be viewed at 
-    string public version = "v0.1.0";              //version
+    uint public releaseTime = 1496423100;       // stores the unix encoded timestamp of release 
+    unit public termOfTrade = 100 ether;        // final contract goal
+    bool public metToT;                         // did contract close
+    uint public listPrice;                      // allow for dynamic pricing
+    address public owner = msg.sender;          // person who is currently able to withdraw
+    address public sponsor = msg.sender;        // person who is putting reputation on the line
+    string public pool;                         // pool url the contract can be viewed at 
+    string public version = "v0.1.1";           // version
 //mapping    
 
 //events
@@ -35,7 +35,16 @@ contract minelock {
 //functions    
     
     //payable
-    function() payable {Funded(msg.sender, msg.value);}             // allow for funding
+    function() payable {                                            // allow for funding
+    Funded(msg.sender, msg.value);
+    if(this.balance + msg.value > termOfTrade){     // stop accepting funds after contract is at the term of trade
+       uint unmet = termOfTrade - this.balance;     // find out the unmet funding goal            
+       uint forward = msg.value - unmet;            // subtrat the unmet from the incoming value
+       sponsor.transfer(forward);                   // forward the excess to the sponsor so no contract will ever exceed the tot
+       forward = 0;                                 // safety reset of local vars
+       unmet = 0;
+       }
+    }             
     
     function trade() payable{                                       //allow the contract to be owner traded
         if(msg.value < listPrice){throw;}
